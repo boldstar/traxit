2ab5f2195b722db3efadbd25296bd6dd1645e868
Thomas <thomasweems3@gmail.com>
2019-05-03 09:54:43 -0500

    added notes to session.js

--- src/plugins/session.js
+++ src/plugins/session.js
@@ -1,6 +1,7 @@
 const moment  = require('moment')
 const axios = require('axios')
 
+// used to check expiration of access token, if expired destroy token else continue.
 export function destroyToken() {
     // get current date stored in local storage
     const expiresOn = localStorage.getItem('expires_on')
@@ -19,6 +20,7 @@ export function destroyToken() {
     };
 }
 
+// use to destroy session if logged out
 export function  destroySession() {
     // I will use this to remove any local storage Items
     localStorage.removeItem('access_token')
@@ -28,20 +30,26 @@ export function  destroySession() {
     return
 }
 
+
+// used to start the session when user logs in
 export function  startSession(response) {
     // I will use this to create the app session just dont know how yet
     const token = response.data.rules.access_token
     const fqdn = response.data.fqdn
     const role = response.data.role[0][0].name
+    // if token and fqdn are available set local storage items and return true
     if(token != null || token != undefined && fqdn != null || fqdn != undefined) {
     localStorage.removeItem('fqdn_api_url')
     const date = new Date(moment().add(1, 'day').toDate());
     localStorage.setItem('fqdn_api_url', fqdn)
     localStorage.setItem('expires_on', date);
     localStorage.setItem('role', role)
+    // I have to set the base url here because it wont pick up the localStorage change until the page is refreshed
+    // So only setting globally at this point does not work
     axios.defaults.headers.common['Authorization'] = 'Bearer ' + token
     axios.defaults.baseURL = 'http://' + response.data.fqdn + '/api'
     return true
+    // if data is not available return false.
     } else {
         return false
     }

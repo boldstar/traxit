aa53202c688bcc688164a7b17fab123951fae55a
Thomas <thomasweems3@gmail.com>
2019-08-06 15:24:54 -0500

    moved invoice to its own component

--- src/children/EngageInvoice.vue
+++ src/children/EngageInvoice.vue
@@ -1,70 +1,22 @@
 <template>
     <div class="mx-auto w-100">
-        <div class="card mt-5" v-if="data">
+        <div class="card mt-5 w-25 mx-auto shadow-sm" v-if="!data">
             <div class="card-body">
                 <i class="fas fa-file-invoice-dollar fa-5x"></i>
             </div>
-            <button class="py-2 btn-primary font-weight-bold" >Create Invoice</button>
+            <button class="py-2 btn-primary font-weight-bold" @click="data = true">Create Invoice</button>
         </div>
-        <div class="invoice w-50 mx-auto">
-            <div class="card">
-                <div class="card-header d-flex">
-                    <div class="flex-fill mr-3">
-                        <div class="input-group mb-2">
-                            <div class="input-group-prepend">
-                                <label class="input-group-text invoice-label font-weight-bold" for="email_to">Bill To</label>
-                            </div>
-                            <input type="text" class="form-control" placeholder="email@example.com">
-                        </div>
-                        <div class="input-group">
-                            <div class="input-group-prepend">
-                                <label class="input-group-text invoice-label font-weight-bold" for="cc">CC</label>
-                            </div>
-                            <input type="text" class="form-control" placeholder="email@example.com">
-                        </div>
-                    </div>
-                    <div class="align-self-center text-primary">
-                         <i class="fas fa-file-invoice-dollar fa-5x"></i>
-                    </div>
-                </div>
-                <div class="card-body p-0">
-                    <table class="table">
-                        <thead>
-                            <tr>
-                                <th>Item</th>
-                                <th>Quantity</th>
-                                <th>Amount</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <th>Item One</th>
-                                <td>1</td>
-                                <td>50</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-                <div class="card-footer d-flex justify-content-between">
-                    <div class="align-self-center">
-                        <button class="btn btn-primary font-weight-bold">Send</button>
-                    </div>
-                    <div class="d-flex flex-column font-weight-bold text-left">
-                        <div class="d-flex mb-2"><span class="align-self-center mr-1">Tax:</span> <input type="text" class="form-control align-self-center tax-input mr-1" placeholder="8.25"><span class="align-self-center">%</span></div>
-                        <div class="d-flex justify-content-between">
-                            <span>Total:</span>
-                            <span class="text-primary">50</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+
+        <Invoice :eng="engagement" v-if="data" @close="data = false" />
     </div>
 </template>
 
 <script>
+import Invoice from '@/components/Invoice.vue'
 export default {
     name: 'EngageInvoice',
+    props: ['engagement'],
+    components: {Invoice},
     data() {
         return {
             data: false
@@ -87,5 +39,10 @@ export default {
 
     .tax-input {
         width: 100px;
+    }
+
+    .amount-input {
+        width: 200px;
+        margin: 0 auto;
     }
 </style>

a7e02ef05d3b3aecfb8a25344c8dc077a084b2bc
Thomas <thomasweems3@gmail.com>
2019-11-22 13:30:06 -0600

    start of portal branch

---
+++ src/store/modules/portal.js
@@ -0,0 +1,18 @@
+import axios from 'axios'
+import moment from 'moment';
+import router from '../../router'
+
+export default {
+    state: {
+
+    },
+    getters: {
+
+    },
+    mutations: {
+
+    },
+    actions: {
+        
+    }
+}
